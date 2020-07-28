9ab04226dfbcc8b9e91a97ca30635594bc5645ec
motovmp1 <vinicius.mpinho@gmail.com>
2020-07-28 20:13:06 +0800

    running test 2

--- remote_driver/chrome_remoto.py
+++ remote_driver/chrome_remoto.py
@@ -8,7 +8,8 @@ options.add_experimental_option("excludeSwitches", ["enable-automation"])
 caps = options.to_capabilities()
 driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub',
                           desired_capabilities=caps)
-
+print(driver.capabilities['version'])
+print(driver.name)
 driver.get('http://www.google.com/')
 print(driver.current_url)
 time.sleep(1)  # Let the user actually see something!
--- remote_driver/remote_teste_firefox.py
+++ remote_driver/remote_teste_firefox.py
@@ -4,6 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 driver = webdriver.Remote('http://localhost:4444/wd/hub', DesiredCapabilities.FIREFOX)
+print(driver.name)
+print(driver.capabilities['browserVersion'])
 driver.get('http://www.google.com/')
 print(driver.current_url)
 time.sleep(1)  # Let the user actually see something!
